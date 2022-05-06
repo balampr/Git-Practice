33bd43e090232e5c2f8765783f0d7410f2678639
balampr <balampr28@gmail.com>
2022-05-06 23:27:41 +0530

    branch and master

---
+++ src/Sample.java
@@ -0,0 +1,16 @@
+
+public class Sample {
+
+	public static void add(int num, int num2) {
+		int s;
+		 s = num + num2;
+		 System.out.println(s);
+
+	}
+
+	public static void main(String[] args) {
+		
+		add(10, 25);
+		
+	}
+}
