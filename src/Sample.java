c7a5e8874a040d20b68d662667168e5781c932ef
balampr <balampr28@gmail.com>
2022-05-09 22:32:29 +0530

    Team-B few lines of code added

--- src/Sample.java
+++ src/Sample.java
@@ -2,15 +2,24 @@
 public class Sample {
 
 	public static void add(int num, int num2) {
-		int s;
-		 s = num + num2;
-		 System.out.println(s);
+		int a;
+		a = num + num2;
+		System.out.println(a);
 
 	}
 
+	private void multi(int num, int num1) {
+           int m;
+           m = num * num1;
+           System.out.println(m);
+           
+	}
+
 	public static void main(String[] args) {
-		
+
 		add(10, 25);
-		
+		Sample mu = new Sample();
+		mu.multi(5, 7);
+
 	}
 }
