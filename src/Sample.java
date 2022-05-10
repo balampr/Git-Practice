5a651e9dd0993346c45bde4ba2f65c8843d70355
balampr <balampr28@gmail.com>
2022-05-10 18:16:13 +0530

    Team-a code was done

--- src/Sample.java
+++ src/Sample.java
@@ -12,11 +12,18 @@ public class Sample {
 		s =num-num2;
 		System.out.println(s);
 	}
+	
+	private void modul(int n,int n1) {
+		int mo = n%n1;
+		System.out.println(mo);
+
+	}
 
 	public static void main(String[] args) {
 		
 		add(10, 25);
-		sub(80, 50);
-		
+		sub(80, 45);
+		Sample mod = new Sample();
+		mod.modul(3035,4);
 	}
 }
